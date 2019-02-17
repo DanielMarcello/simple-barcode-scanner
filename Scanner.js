d7d1a151afb2bca8873e2b236016a711d24c8a95
Daniel Salinas <daniel.salinas.tellez@gmail.com>
2019-02-17 17:58:47 -0500

    Adds inputText with verification button

--- Scanner.js
+++ Scanner.js
@@ -6,7 +6,12 @@ import {
   Text,
   Dimensions,
   Alert,
-  Linking
+  Linking,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+  TouchableHighlight,
+  Image
 } from "react-native";
 import { Constants, BarCodeScanner, Camera, Permissions } from "expo";
 
@@ -16,7 +21,9 @@ class Scanner extends Component {
 
     this.state = {
       hasCameraPermission: null,
-      pendingAction: false
+      pendingAction: false,
+      inputText: "",
+      enabledInput: true
     };
   }
 
@@ -32,28 +39,30 @@ class Scanner extends Component {
   };
 
   _handleBarCodeRead = data => {
-    if (!this.state.pendingAction) {
-      this.setState({ pendingAction: !this.state.pendingAction });
-      Alert.alert(
-        "Scan successful!",
-        JSON.stringify(data),
-        [
-          {
-            text: "Cancel",
-            onPress: () =>
-              this.setState({ pendingAction: !this.state.pendingAction }),
-            style: "cancel"
-          },
-          {
-            text: "OK",
-            onPress: () => {
-              this.setState({ pendingAction: !this.state.pendingAction });
-              Linking.openURL("https://google.com");
+    if (!this.state.enabledInput) {
+      if (!this.state.pendingAction) {
+        this.setState({ pendingAction: !this.state.pendingAction });
+        Alert.alert(
+          this.state.TextInput,
+          JSON.stringify(data),
+          [
+            {
+              text: "Cancel",
+              onPress: () =>
+                this.setState({ pendingAction: !this.state.pendingAction }),
+              style: "cancel"
+            },
+            {
+              text: "OK",
+              onPress: () => {
+                this.setState({ pendingAction: !this.state.pendingAction });
+                Linking.openURL("https://google.com");
+              }
             }
-          }
-        ],
-        { cancelable: false }
-      );
+          ],
+          { cancelable: false }
+        );
+      }
     }
   };
 
@@ -65,11 +74,42 @@ class Scanner extends Component {
         ) : this.state.hasCameraPermission === false ? (
           <Text>Camera permission is not granted</Text>
         ) : (
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            flashMode={Camera.Constants.FlashMode.auto}
-            style={{ height: "100%", width: Dimensions.get("window").width }}
-          />
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              flashMode={Camera.Constants.FlashMode.auto}
+              style={{ height: "100%", width: Dimensions.get("window").width }}
+            >
+              <View style={styles.inputView}>
+                <TextInput
+                  style={styles.inputText}
+                  onChangeText={text => this.setState({ TextInput: text })}
+                  value={this.state.text}
+                  placeholder="Type list code here..."
+                  editable={this.state.enabledInput}
+                />
+                <TouchableHighlight
+                  style={styles.inputTouchable}
+                  onPress={() => {
+                    Keyboard.dismiss();
+                    this.setState({ enabledInput: !this.state.enabledInput });
+                  }}
+                >
+                  {!this.state.enabledInput ? (
+                    <Image
+                      style={styles.inputImage}
+                      source={require("./static/img/greenCheck.png")}
+                    />
+                  ) : (
+                    <Image
+                      style={styles.inputImage}
+                      source={require("./static/img/grayCheck.png")}
+                    />
+                  )}
+                </TouchableHighlight>
+              </View>
+            </BarCodeScanner>
+          </TouchableWithoutFeedback>
         )}
       </View>
     );
@@ -83,6 +123,30 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
     backgroundColor: "#ecf0f1"
+  },
+  inputView: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    marginTop: 10
+  },
+  inputText: {
+    height: 40,
+    width: "80%",
+    backgroundColor: "white",
+    alignSelf: "center",
+    paddingHorizontal: 10,
+    flex: 4
+  },
+  inputTouchable: {
+    flex: 1,
+    alignItems: "stretch",
+    padding: 2,
+    backgroundColor: "white"
+  },
+  inputImage: {
+    flex: 1,
+    width: null,
+    height: null
   }
 });
 
